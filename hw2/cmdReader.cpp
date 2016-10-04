c11a38f30986ddc7b6f5b4c581988ab998f6fb08
davidkchou <d040000@gmail.com>
2016-10-04 20:39:57 +0800

    Add files via upload

--- hw2/cmdReader.cpp
+++ hw2/cmdReader.cpp
@@ -48,7 +48,7 @@ CmdParser::readCmdInt(istream& istr)
          case HOME_KEY       : moveBufPtr(_readBuf); break;
          case LINE_END_KEY   :
          case END_KEY        : moveBufPtr(_readBufEnd); break;
-         case BACK_SPACE_KEY : moveBufPtr(_readBufPtr-1);
+         case BACK_SPACE_KEY : if(moveBufPtr(_readBufPtr-1))
                                deleteChar();
                                break;
          case DELETE_KEY     : deleteChar(); break;
@@ -61,7 +61,13 @@ CmdParser::readCmdInt(istream& istr)
          case ARROW_LEFT_KEY : moveBufPtr(_readBufPtr-1); break;
          case PG_UP_KEY      : moveToHistory(_historyIdx - PG_OFFSET); break;
          case PG_DOWN_KEY    : moveToHistory(_historyIdx + PG_OFFSET); break;
-         case TAB_KEY        : insertChar(' ', TAB_POSITION); break;
+         case TAB_KEY        : {int count=0;
+         					   char* temp_p=_readBufPtr;
+         					   while(temp_p!=_readBuf){
+         					   		temp_p--;
+         					   		count++;
+         					   }
+         					   insertChar(' ', TAB_POSITION-(count%TAB_POSITION)); break;}
          case INSERT_KEY     : // not yet supported; fall through to UNDEFINE
          case UNDEFINED_KEY:   mybeep(); break;
          default:  // printable character
@@ -73,28 +79,18 @@ CmdParser::readCmdInt(istream& istr)
    }
 }
 
-
-// This function moves _readBufPtr to the "ptr" pointer
-// It is used by left/right arrowkeys, home/end, etc.
-//
-// Suggested steps:
-// 1. Make sure ptr is within [_readBuf, _readBufEnd].
-//    If not, make a beep sound and return false. (DON'T MOVE)
-// 2. Move the cursor to the left or right, depending on ptr
-// 3. Update _readBufPtr accordingly. The content of the _readBuf[] will
-//    not be changed
-//
-// [Note] This function can also be called by other member functions below
-//        to move the _readBufPtr to proper position.
 bool
 CmdParser::moveBufPtr(char* const ptr)
 {
-   //vTODO...
   if(ptr>_readBufEnd || ptr<_readBuf){
+  
     mybeep();
-    return true;
+    
+    return false;
   }
+  
   else{
+  
   while(_readBufPtr!=ptr){
 
     if(ptr<_readBufPtr){
@@ -115,30 +111,9 @@ CmdParser::moveBufPtr(char* const ptr)
  }
 }
 
-// [Notes]
-// 1. Delete the char at _readBufPtr
-// 2. mybeep() and return false if at _readBufEnd
-// 3. Move the remaining string left for one character
-// 4. The cursor should stay at the same position
-// 5. Remember to update _readBufEnd accordingly.
-// 6. Don't leave the tailing character.
-// 7. Call "moveBufPtr(...)" if needed.
-//
-// For example,
-//
-// cmd> This is the command
-//              ^                (^ is the cursor position)
-//
-// After calling deleteChar()---
-//
-// cmd> This is he command
-//              ^
-//
 bool
 CmdParser::deleteChar()
 {
-   // vTODO...
-
    if(_readBufPtr!=_readBufEnd){
 
          char* temp_ptr=_readBufPtr;
@@ -173,6 +148,14 @@ CmdParser::deleteChar()
         for(int i=1;i<(count+1);i++)
 
           cout<<'\b';
+      	/*
+        if(_tempCmdStored){
+          
+          _history.pop_back();
+
+          _tempCmdStored = false;
+        }
+        */
    }
 
    else
@@ -182,25 +165,9 @@ CmdParser::deleteChar()
    return true;
 }
 
-// 1. Insert character 'ch' for "repeat" times at _readBufPtr
-// 2. Move the remaining string right for "repeat" characters
-// 3. The cursor should move right for "repeats" positions afterwards
-// 4. Default value for "repeat" is 1. You should assert that (repeat >= 1).
-//
-// For example,
-//
-// cmd> This is the command
-//              ^                (^ is the cursor position)
-//
-// After calling insertChar('k', 3) ---
-//
-// cmd> This is kkkthe command
-//                 ^
-//
 void
 CmdParser::insertChar(char ch, int repeat)
 {
-   // vTODO...
     assert(repeat >= 1);
 
     for(int i=0; i<repeat; i++){
@@ -214,7 +181,6 @@ CmdParser::insertChar(char ch, int repeat)
           cout<< *_readBufPtr;
 
           _readBufPtr++;
-
       }
 
       else{
@@ -243,7 +209,6 @@ CmdParser::insertChar(char ch, int repeat)
           temp_c = next_c;
 
           count++;
-
         }
 
         _readBufPtr = temp_ptr;
@@ -256,31 +221,22 @@ CmdParser::insertChar(char ch, int repeat)
         _readBufPtr = temp_ptr;
 
         _readBufPtr++;
-
       }  
     }
+    /*
+    if(_tempCmdStored){
+        
+        _history.pop_back();
 
+        _tempCmdStored = false;
+      }
+      */
 }
 
-// 1. Delete the line that is currently shown on the screen
-// 2. Reset _readBufPtr and _readBufEnd to _readBuf
-// 3. Make sure *_readBufEnd = 0
-//
-// For example,
-//
-// cmd> This is the command
-//              ^                (^ is the cursor position)
-//
-// After calling deleteLine() ---
-//
-// cmd>
-//      ^
-//
 void
 CmdParser::deleteLine()
 {
-     // vTODO...
-
+    //cout<<"in delete line";
     while(_readBufPtr!=_readBuf){
 
       cout<<'\b';
@@ -291,7 +247,6 @@ CmdParser::deleteLine()
     while(_readBufPtr!=_readBufEnd){
 
       cout<<' ';
-
       _readBufPtr++;
     }
 
@@ -300,48 +255,54 @@ CmdParser::deleteLine()
       cout<<'\b';
 
       _readBufPtr--;
+
+      *(_readBufPtr+1) = '\0';
     }
-
-
 }
 
-
-// This functions moves _historyIdx to index and display _history[index]
-// on the screen.
-//
-// Need to consider:
-// If moving up... (i.e. index < _historyIdx)
-// 1. If already at top (i.e. _historyIdx == 0), beep and do nothing.
-// 2. If at bottom, temporarily record _readBuf to history.
-//    (Do not remove spaces, and set _tempCmdStored to "true")
-// 3. If index < 0, let index = 0.
-//
-// If moving down... (i.e. index > _historyIdx)
-// 1. If already at bottom, beep and do nothing
-// 2. If index >= _history.size(), let index = _history.size() - 1.
-//
-// Assign _historyIdx to index at the end.
-//
-// [Note] index should not = _historyIdx
-//
 void
 CmdParser::moveToHistory(int index)
 {
   if(!_tempCmdStored){
 
-   if(index<0 || index>_history.size())
+   if(index<0){
+     
+      if(_historyIdx==0)
+     
+       mybeep();
+     
+      else{
+     
+        index = 0;
+     
+        _historyIdx = index;
+     
+        retrieveHistory();
+      }
+    }
+   
+   else if(index>_history.size()){
+   
+      if(_historyIdx==_history.size())
+   
+       mybeep();
+   
+      else{
+   
+        index = _history.size();
 
-      mybeep();
+        _historyIdx = index;
+        
+        retrieveHistory();
+      }
+    }
 
    else{
-
-      if(_historyIdx == _history.size()){
 
         _history.push_back(_readBuf);
 
         _tempCmdStored = true;
 
-      }
         _historyIdx = index;
       
         retrieveHistory();
@@ -350,89 +311,94 @@ CmdParser::moveToHistory(int index)
 
   else{
 
-     if(index<0 || index>(_history.size()-1))
+     if(index<0){
+        
+        if(_historyIdx==0)
+        
+          mybeep();
+        
+        else{
+        
+          index = 0;
+        
+          _historyIdx = index;
+        
+          retrieveHistory();
+        }
+      }
 
-        mybeep();
+      else if (index>(_history.size()-1)){
+      
+        if(_historyIdx==(_history.size()-1))
+      
+          mybeep();
+      
+        else{
+      
+          index = _history.size()-1;
+      
+          _historyIdx = index;
+      
+          retrieveHistory();
+        }
+      }
 
       else{
+      	if(index == (_history.size()-1)){
+      		_history.pop_back();
+      		_tempCmdStored = false;
+      	}
 
         _historyIdx = index;
-        
-        if(_historyIdx == (_history.size()-1)){
-            
-            _history.pop_back();
-            
-            _tempCmdStored = false;
-
-            _readBufPtr = _readBuf;
-
-              while(_readBufPtr!=_readBufEnd){
-
-                *_readBufPtr='\0';
-
-                _readBufPtr++;
-              }
-          }
-
+          
         retrieveHistory();
-
-        
-
       }
     }
 }
 
-// This function adds the string in _readBuf to the _history.
-// The size of _history may or may not change. Depending on whether 
-// there is a temp history string.
-//
-// 1. Remove ' ' at the beginning and end of _readBuf
-// 2. If not a null string, add string to _history.
-//    Be sure you are adding to the right entry of _history.
-// 3. If it is a null string, don't add anything to _history.
-// 4. Make sure to clean up "temp recorded string" (added earlier by up/pgUp,
-//    and reset _tempCmdStored to false
-// 5. Reset _historyIdx to _history.size() // for future insertion
-//
 void
 CmdParser::addHistory()
 {
-   // TODO...
     if(_tempCmdStored){
 
       _history.pop_back();
 
-      _tempCmdStored = true;
+      _tempCmdStored = false;
     }
-    
-
     //check space
+     while(*_readBuf==' '){
         
-        while(*_readBuf==' '){
-          char* next_ptr;
-          _readBufPtr = _readBuf;
-          next_ptr = _readBufPtr;
-          next_ptr++;
-          while(_readBufPtr!=_readBufEnd){
-            *_readBufPtr=*next_ptr;
-            _readBufPtr++;
-            next_ptr++;
-          }
-          _readBufEnd--;
-          *_readBufEnd = '\0';
-
+       char* next_ptr;
+          
+       _readBufPtr = _readBuf;
+          
+       next_ptr = _readBufPtr;
+          
+       next_ptr++;
+          
+       while(_readBufPtr!=_readBufEnd){
+          
+         *_readBufPtr=*next_ptr;
+          
+         _readBufPtr++;
+          
+         next_ptr++;
+        }
+          
+        _readBufEnd--;
+          
+        *_readBufEnd = '\0';
         }
    
         while(*(_readBufEnd-1)==' '){
+          
           _readBufEnd--;
+          
           *_readBufEnd = '\0';
         }
       
-
-
-   
-
    if(_readBuf!=_readBufEnd)  
+      
       _history.push_back(_readBuf);
 
     _readBufPtr = _readBuf;
@@ -447,15 +413,8 @@ CmdParser::addHistory()
     _readBufPtr = _readBufEnd = _readBuf;
 
     _historyIdx = _history.size();
-
 }
 
-
-// 1. Replace current line with _history[_historyIdx] on the screen
-// 2. Set _readBufPtr and _readBufEnd to end of line
-//
-// [Note] Do not change _history.size().
-//
 void
 CmdParser::retrieveHistory()
 {
